3571089e833c2e2711df9efe37b78e69052753be
mark <mark.korput@gmail.com>
2019-08-22 22:21:53 +0200

    Runtime.createInstance

--- include/info/Runtime.h
+++ include/info/Runtime.h
@@ -1,16 +1,29 @@
 #pragma once
 
+#include <functional>
 #include <stdlib.h>
+#include <iostream>
+#include <memory>
 #include "TypeBuilder.hpp"
 #include "Type.h"
+#include "Instance.h"
 
 namespace info {
   class Runtime {
     public:
+
       template<class T>
       std::shared_ptr<Type> addType(const std::string& typeId, std::function<void(TypeBuilder<T>&)> builderFunc) {
         auto typeRef = Type::create(typeId, builderFunc);
         typeRefs.push_back(typeRef);
+
+        typeInstantiatorFuncs[typeRef.get()] = [typeRef](){
+          auto object = new T();
+          return typeRef->template createInstance<T>(*object);
+        };
+
+        std::cout << "TODO: also register an on destroy func" << std::endl;
+
         return typeRef;
       }
 
@@ -18,7 +31,34 @@ namespace info {
         return typeRefs;
       }
 
+      std::shared_ptr<Instance> createInstance(const std::string& typeId) {
+        // find registred type
+        auto typeRef = getType(typeId);
+        if (!typeRef) return nullptr;
+
+        // find instatiator func that belongs to the found type
+        auto pair = typeInstantiatorFuncs.find(typeRef.get());
+        if (pair == typeInstantiatorFuncs.end()) return nullptr; // no instantiator?
+
+        // create, save and return
+        auto intantiatorFunc = pair->second;
+        auto instanceRef = intantiatorFunc();
+        instanceRefs.push_back(instanceRef);
+        return instanceRef;
+      }
+
+    protected:
+
+      std::shared_ptr<Type> getType(const std::string& typeId) {
+        for(auto typeRef : typeRefs)
+          if (typeRef->getId() == typeId)
+            return typeRef;
+        return nullptr;
+      }
+
     private:
       std::vector<std::shared_ptr<Type>> typeRefs;
+      std::map<Type*, std::function<std::shared_ptr<Instance>()>> typeInstantiatorFuncs;
+      std::vector<std::shared_ptr<Instance>> instanceRefs;
   };
 }
--- tests/src/test_runtime.cpp
+++ tests/src/test_runtime.cpp
@@ -9,15 +9,45 @@
 
 TEST_CASE("info::Runtime", ""){
 
-
   SECTION("addType"){
     info::Runtime runtime;
 
     REQUIRE(runtime.getTypes().size() == 0);
+    
     auto type = runtime.addType<bool>("bool", [](info::TypeBuilder<bool>& builder){
     });
 
     REQUIRE(runtime.getTypes().size() == 1);
     REQUIRE(runtime.getTypes()[0]->getId() == "bool");
+  }
+
+  SECTION("createInstance"){
+    info::Runtime runtime;
+
+    auto type = runtime.addType<bool>("bool", [](info::TypeBuilder<bool>& builder){
+      builder.attr<bool>("value")
+        ->apply([](bool& instance, info::TypedPort<bool>& port) {
+          // when data comes in through the value-IN-port; apply that value
+          port.onDataIn([&instance, &port](const bool& val){
+            if (val == instance) return;
+            instance = val;
+             // immediately send the new value to the out port
+            port.dataOut(instance);
+          });
+        });
+    });
+
+    std::shared_ptr<info::Instance> instanceRef = runtime.createInstance("bool");
+
+    bool feedback = false;
+    instanceRef->port<bool>("value")->onDataOut([&feedback](const bool& val){
+      feedback = val;
+    });
+
+    REQUIRE(feedback == false);
+    instanceRef->port<bool>("value")->dataIn(true);
+    REQUIRE(feedback == true);
+    instanceRef->port<bool>("value")->dataIn(false);
+    REQUIRE(feedback == false);
   }
 }
